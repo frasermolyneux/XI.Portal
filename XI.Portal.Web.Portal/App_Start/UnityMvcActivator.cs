eae1a77eed220395d9086c52672da69415d5d9a6
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2019-05-26 21:29:38 +0100

    Updating to latest NuGet packages

--- XI.Portal.Web.Portal/App_Start/UnityMvcActivator.cs
+++ XI.Portal.Web.Portal/App_Start/UnityMvcActivator.cs
@@ -3,21 +3,33 @@ using System.Web.Mvc;
 
 using Unity.AspNet.Mvc;
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(XI.Portal.Web.UnityMvcActivator), nameof(XI.Portal.Web.UnityMvcActivator.Start))]
-[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(XI.Portal.Web.UnityMvcActivator), nameof(XI.Portal.Web.UnityMvcActivator.Shutdown))]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(XI.Portal.Web.Portal.UnityMvcActivator), nameof(XI.Portal.Web.Portal.UnityMvcActivator.Start))]
+[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(XI.Portal.Web.Portal.UnityMvcActivator), nameof(XI.Portal.Web.Portal.UnityMvcActivator.Shutdown))]
 
-namespace XI.Portal.Web
+namespace XI.Portal.Web.Portal
 {
+    /// <summary>
+    /// Provides the bootstrapping for integrating Unity with ASP.NET MVC.
+    /// </summary>
     public static class UnityMvcActivator
     {
+        /// <summary>
+        /// Integrates Unity when the application starts.
+        /// </summary>
         public static void Start() 
         {
             FilterProviders.Providers.Remove(FilterProviders.Providers.OfType<FilterAttributeFilterProvider>().First());
             FilterProviders.Providers.Add(new UnityFilterAttributeFilterProvider(UnityConfig.Container));
 
             DependencyResolver.SetResolver(new UnityDependencyResolver(UnityConfig.Container));
+
+            // TODO: Uncomment if you want to use PerRequestLifetimeManager
+            // Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(UnityPerRequestHttpModule));
         }
 
+        /// <summary>
+        /// Disposes the Unity container when the application is shut down.
+        /// </summary>
         public static void Shutdown()
         {
             UnityConfig.Container.Dispose();
