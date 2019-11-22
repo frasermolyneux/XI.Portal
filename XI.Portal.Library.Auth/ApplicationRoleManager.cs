605cb0e55e4cf74889cad954bcc9bf33e9f17848
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2019-11-22 14:05:34 +0000

    Fixed a bug if the role doesn;t exist in the database

---
+++ XI.Portal.Library.Auth/ApplicationRoleManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace XI.Portal.Library.Auth
+{
+    public class ApplicationRoleManager : RoleManager<IdentityRole>, IDisposable
+    {
+        public ApplicationRoleManager(RoleStore<IdentityRole> store) : base(store) { }
+    }
+}
