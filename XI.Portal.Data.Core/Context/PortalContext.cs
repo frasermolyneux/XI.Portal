2891aee5c81a7fd4231db88f70f4c74e2be5ecf4
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2019-11-23 13:10:27 +0000

    Added a basic map vote plugin and structure

--- XI.Portal.Data.Core/Context/PortalContext.cs
+++ XI.Portal.Data.Core/Context/PortalContext.cs
@@ -25,9 +25,10 @@ namespace XI.Portal.Data.Core.Context
         public DbSet<RconMonitor> RconMonitors { get; set; }
         public DbSet<Map> Maps { get; set; }
         public DbSet<MapFile> MapFiles { get; set; }
+        public DbSet<MapRotation> MapRotations { get; set; }
+        public DbSet<MapVote> MapVotes { get; set; }
         public DbSet<UserLog> UserLogs { get; set; }
         public DbSet<SystemLog> SystemLogs { get; set; }
-        public DbSet<MapRotation> MapRotations { get; set; }
         public DbSet<AdminAction> AdminActions { get; set; }
         public DbSet<PlayerIpAddress> PlayerIpAddresses { get; set; }
         public DbSet<PlayerAlias> PlayerAliases { get; set; }
---
+++ XI.Portal.Data.Core/Models/MapVote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace XI.Portal.Data.Core.Models
+{
+    public class MapVote
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Index(IsUnique = true)]
+        public Guid MapVoteId { get; set; }
+
+        public Player2 Player { get; set; }
+        public Map Map { get; set; }
+        public bool Like { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
