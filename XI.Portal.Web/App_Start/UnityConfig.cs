66bdabf35e5c8310e828cc72a5c6a4554478c739
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2019-12-02 19:25:54 +0000

    Regenerating binding redirects using Add-BindingRedirect and fixed Unity registrations on web project

--- XI.Portal.Web/App_Start/UnityConfig.cs
+++ XI.Portal.Web/App_Start/UnityConfig.cs
@@ -1,6 +1,33 @@
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using Serilog;
 using System;
-
+using System.Data.Entity;
+using System.Web;
 using Unity;
+using Unity.Injection;
+using Unity.Lifetime;
+using XI.Portal.Configuration.AwsSecrets;
+using XI.Portal.Configuration.Database;
+using XI.Portal.Configuration.Demos;
+using XI.Portal.Configuration.Forums;
+using XI.Portal.Configuration.GeoLocation;
+using XI.Portal.Configuration.Interfaces;
+using XI.Portal.Configuration.LogProxyPlugin;
+using XI.Portal.Configuration.Maps;
+using XI.Portal.Configuration.Providers;
+using XI.Portal.Data.Core.Context;
+using XI.Portal.Data.Core.Models;
+using XI.Portal.Library.Auth;
+using XI.Portal.Library.Forums;
+using XI.Portal.Library.GameTracker;
+using XI.Portal.Library.GeoLocation.Repository;
+using XI.Portal.Library.Logging;
+using XI.Portal.Library.MapRedirect;
+using XI.Portal.Library.Rcon.Factories;
+using XI.Portal.Library.Rcon.Interfaces;
+using XI.Portal.Web.Navigation;
 
 namespace XI.Portal.Web
 {
@@ -36,12 +63,52 @@ namespace XI.Portal.Web
         /// </remarks>
         public static void RegisterTypes(IUnityContainer container)
         {
-            // NOTE: To load from web.config uncomment the line below.
-            // Make sure to add a Unity.Configuration to the using statements.
-            // container.LoadConfiguration();
+            var logger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .WriteTo.ColoredConsole()
+                .CreateLogger();
 
-            // TODO: Register your type's mappings here.
-            // container.RegisterType<IProductRepository, ProductRepository>();
+            Log.Logger = logger;
+
+            container.RegisterFactory<ILogger>((ctr, type, name) => logger, new ContainerControlledLifetimeManager());
+
+            // Configuration Providers
+            container.RegisterType<IConfigurationProvider, ConfigurationProvider>();
+            container.RegisterType<IAwsSecretConfigurationProvider, AwsSecretConfigurationProvider>();
+            container.RegisterType<ILocalConfigurationProvider, LocalConfigurationProvider>();
+
+            // Configurations
+            container.RegisterType<IAwsSecretsConfiguration, AwsSecretsConfiguration>();
+            container.RegisterType<IDatabaseConfiguration, DatabaseConfiguration>();
+            container.RegisterType<IDemosConfiguration, DemosConfiguration>();
+            container.RegisterType<IForumsConfiguration, ForumsConfiguration>();
+            container.RegisterType<IGeoLocationConfiguration, GeoLocationConfiguration>();
+            container.RegisterType<ILogProxyPluginConfiguration, LogProxyPluginConfiguration>();
+            container.RegisterType<IMapsConfiguration, MapsConfiguration>();
+
+            // Other
+            container.RegisterType<IContextProvider, ContextProvider>();
+            container.RegisterType<INavigationMenu, NavigationMenu>();
+            container.RegisterType<IMapImageRepository, MapImageRepository>();
+            container.RegisterType<IDatabaseLogger, DatabaseLogger>();
+            container.RegisterType<IGeoLocationApiRepository, GeoLocationApiRepository>();
+            container.RegisterType<IMapRedirectRepository, MapRedirectRepository>();
+            container.RegisterType<IManageTopics, ManageTopics>();
+            container.RegisterType<IRconClientFactory, RconClientFactory>();
+
+            var contextProvider = container.Resolve<IDatabaseConfiguration>();
+            container.RegisterType<DbContext, PortalContext>(new InjectionConstructor(contextProvider.PortalDbConnectionString));
+            var context = container.Resolve<DbContext>();
+
+            container.RegisterType<ApplicationSignInManager>();
+            container.RegisterType<ApplicationUserManager>();
+            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>();
+            container.RegisterType<UserManager<ApplicationUser>>(new HierarchicalLifetimeManager());
+
+            container.RegisterFactory<ApplicationRoleManager>(x => new ApplicationRoleManager(new RoleStore<IdentityRole>(context)));
+
+            container.RegisterFactory<IdentityRole>(x => new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context)));
+            container.RegisterFactory<IAuthenticationManager>(x => HttpContext.Current.GetOwinContext().Authentication);
         }
     }
 }
