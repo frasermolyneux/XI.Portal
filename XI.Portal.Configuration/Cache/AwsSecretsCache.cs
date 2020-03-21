257fd780c88b2f0c28cbeebdfc8cd3c9db0f0c0f
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-21 14:18:53 +0000

    Adding in a very simple cache on the aws secrets configuration provider as it is spamming the secrets manager.

---
+++ XI.Portal.Configuration/Cache/AwsSecretsCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XI.Portal.Configuration.Cache
+{
+    public static class AwsSecretsCache
+    {
+        public static Dictionary<string, string> CachedSecrets = new Dictionary<string, string>();
+        public static DateTime Cached { get; set; } = DateTime.MinValue;
+    }
+}
