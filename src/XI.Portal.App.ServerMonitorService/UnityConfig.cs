6d05d3f46960ddbd82b12412a7964ea942a0edc2
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-22 14:20:14 +0000

    Restructure, adding unity separation and service recovery

---
+++ src/XI.Portal.App.ServerMonitorService/UnityConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Serilog;
+using Unity;
+using Unity.Lifetime;
+using XI.Portal.Configuration.AwsSecrets;
+using XI.Portal.Configuration.Database;
+using XI.Portal.Configuration.Demos;
+using XI.Portal.Configuration.Forums;
+using XI.Portal.Configuration.Interfaces;
+using XI.Portal.Configuration.LogProxyPlugin;
+using XI.Portal.Configuration.Maps;
+using XI.Portal.Configuration.Providers;
+using XI.Portal.Data.Core.Context;
+using XI.Portal.Library.Logging;
+
+namespace XI.Portal.App.ServerMonitorService
+{
+    public static class UnityConfig
+    {
+        private static readonly Lazy<IUnityContainer> container =
+            new Lazy<IUnityContainer>(() =>
+            {
+                var container = new UnityContainer();
+                RegisterTypes(container);
+                return container;
+            });
+
+        public static IUnityContainer Container => container.Value;
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            var logger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .WriteTo.ColoredConsole()
+                .CreateLogger();
+            Log.Logger = logger;
+
+            container.RegisterFactory<ILogger>((ctr, type, name) => logger, new ContainerControlledLifetimeManager());
+
+            // Configuration Providers
+            container.RegisterType<IConfigurationProvider, ConfigurationProvider>();
+            container.RegisterType<IAwsSecretConfigurationProvider, AwsSecretConfigurationProvider>();
+            container.RegisterType<ILocalConfigurationProvider, LocalConfigurationProvider>();
+
+            // Configurations
+            container.RegisterType<IAwsSecretsConfiguration, AwsSecretsConfiguration>();
+            container.RegisterType<IDatabaseConfiguration, DatabaseConfiguration>();
+            container.RegisterType<IDemosConfiguration, DemosConfiguration>();
+            container.RegisterType<IForumsConfiguration, ForumsConfiguration>();
+            container.RegisterType<ILogProxyPluginConfiguration, LogProxyPluginConfiguration>();
+            container.RegisterType<IMapsConfiguration, MapsConfiguration>();
+
+            // Other
+            container.RegisterType<IContextProvider, ContextProvider>();
+            container.RegisterType<IDatabaseLogger, DatabaseLogger>();
+        }
+    }
+}
