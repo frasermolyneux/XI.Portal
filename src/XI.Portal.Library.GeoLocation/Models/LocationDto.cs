bf6145e73ef6cf63b2ed41eec16c76cdd9d398d9
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-22 11:42:06 +0000

    WIP - removing old geo location library

--- src/XI.Portal.Library.GeoLocation/Models/LocationDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace XI.Portal.Library.GeoLocation.Models
-{
-    public class LocationDto
-    {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-    }
-}
