d6d84d4b5552d79a778c6272f617c3177948dc11
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-22 14:17:48 +0000

    Adding unity container separation and service recovery options

--- src/XI.Portal.App.RconMonitorService/Program.cs
+++ src/XI.Portal.App.RconMonitorService/Program.cs
@@ -1,27 +1,8 @@
 ﻿using System;
-using FM.GeoLocation.Client;
 using Serilog;
 using Topshelf;
 using Topshelf.Unity;
 using Unity;
-using Unity.Lifetime;
-using XI.Portal.App.RconMonitorService.Factories;
-using XI.Portal.App.RconMonitorService.Interfaces;
-using XI.Portal.Configuration.AwsSecrets;
-using XI.Portal.Configuration.Database;
-using XI.Portal.Configuration.Demos;
-using XI.Portal.Configuration.Forums;
-using XI.Portal.Configuration.GeoLocation;
-using XI.Portal.Configuration.Interfaces;
-using XI.Portal.Configuration.LogProxyPlugin;
-using XI.Portal.Configuration.Maps;
-using XI.Portal.Configuration.Providers;
-using XI.Portal.Data.Core.Context;
-using XI.Portal.Library.Logging;
-using XI.Portal.Library.Rcon.Factories;
-using XI.Portal.Library.Rcon.Interfaces;
-using XI.Portal.Plugins.PlayerInfoPlugin.Interfaces;
-using XI.Portal.Plugins.PlayerInfoPlugin.LocalCaching;
 
 namespace XI.Portal.App.RconMonitorService
 {
@@ -29,39 +10,8 @@ namespace XI.Portal.App.RconMonitorService
     {
         private static void Main()
         {
-            var container = new UnityContainer();
-
-            var logger = new LoggerConfiguration()
-                .ReadFrom.AppSettings()
-                .WriteTo.ColoredConsole()
-                .CreateLogger();
-            Log.Logger = logger;
-
-            container.RegisterFactory<ILogger>((ctr, type, name) => logger, new ContainerControlledLifetimeManager());
-
-            // Configuration Providers
-            container.RegisterType<IConfigurationProvider, ConfigurationProvider>();
-            container.RegisterType<IAwsSecretConfigurationProvider, AwsSecretConfigurationProvider>();
-            container.RegisterType<ILocalConfigurationProvider, LocalConfigurationProvider>();
-
-            // Configurations
-            container.RegisterType<IAwsSecretsConfiguration, AwsSecretsConfiguration>();
-            container.RegisterType<IDatabaseConfiguration, DatabaseConfiguration>();
-            container.RegisterType<IDemosConfiguration, DemosConfiguration>();
-            container.RegisterType<IForumsConfiguration, ForumsConfiguration>();
-            container.RegisterType<ILogProxyPluginConfiguration, LogProxyPluginConfiguration>();
-            container.RegisterType<IMapsConfiguration, MapsConfiguration>();
-
-            // Other
-            container.RegisterType<IContextProvider, ContextProvider>();
-            container.RegisterType<IDatabaseLogger, DatabaseLogger>();
-            container.RegisterType<IRconClientFactory, RconClientFactory>();
-            container.RegisterType<IRconMonitorFactory, RconMonitorFactory>();
-            container.RegisterType<IPlayerCaching, PlayerCaching>();
-
-            // FM.GeoLocation
-            container.RegisterType<IGeoLocationClientConfiguration, GeoLocationClientConfig>();
-            container.RegisterType<IGeoLocationClient, GeoLocationClient>();
+            var container = (UnityContainer) UnityConfig.Container;
+            var logger = container.Resolve<ILogger>();
 
             HostFactory.Run(x =>
             {
@@ -78,6 +28,10 @@ namespace XI.Portal.App.RconMonitorService
 
                 x.RunAsLocalSystem();
                 x.UseAssemblyInfoForServiceInfo();
+
+                x.EnableServiceRecovery(service =>
+                    service.RestartService(TimeSpan.FromSeconds(30))
+                );
 
                 x.OnException(ex =>
                 {
---
+++ src/XI.Portal.App.RconMonitorService/UnityConfig.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using FM.GeoLocation.Client;
+using Serilog;
+using Unity;
+using Unity.Lifetime;
+using XI.Portal.App.RconMonitorService.Factories;
+using XI.Portal.App.RconMonitorService.Interfaces;
+using XI.Portal.Configuration.AwsSecrets;
+using XI.Portal.Configuration.Database;
+using XI.Portal.Configuration.Demos;
+using XI.Portal.Configuration.Forums;
+using XI.Portal.Configuration.GeoLocation;
+using XI.Portal.Configuration.Interfaces;
+using XI.Portal.Configuration.LogProxyPlugin;
+using XI.Portal.Configuration.Maps;
+using XI.Portal.Configuration.Providers;
+using XI.Portal.Data.Core.Context;
+using XI.Portal.Library.Logging;
+using XI.Portal.Library.Rcon.Factories;
+using XI.Portal.Library.Rcon.Interfaces;
+using XI.Portal.Plugins.PlayerInfoPlugin.Interfaces;
+using XI.Portal.Plugins.PlayerInfoPlugin.LocalCaching;
+
+namespace XI.Portal.App.RconMonitorService
+{
+    public static class UnityConfig
+    {
+        private static readonly Lazy<IUnityContainer> container =
+            new Lazy<IUnityContainer>(() =>
+            {
+                var container = new UnityContainer();
+                RegisterTypes(container);
+                return container;
+            });
+
+        public static IUnityContainer Container => container.Value;
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            var logger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .WriteTo.ColoredConsole()
+                .CreateLogger();
+            Log.Logger = logger;
+
+            container.RegisterFactory<ILogger>((ctr, type, name) => logger, new ContainerControlledLifetimeManager());
+
+            // Configuration Providers
+            container.RegisterType<IConfigurationProvider, ConfigurationProvider>();
+            container.RegisterType<IAwsSecretConfigurationProvider, AwsSecretConfigurationProvider>();
+            container.RegisterType<ILocalConfigurationProvider, LocalConfigurationProvider>();
+
+            // Configurations
+            container.RegisterType<IAwsSecretsConfiguration, AwsSecretsConfiguration>();
+            container.RegisterType<IDatabaseConfiguration, DatabaseConfiguration>();
+            container.RegisterType<IDemosConfiguration, DemosConfiguration>();
+            container.RegisterType<IForumsConfiguration, ForumsConfiguration>();
+            container.RegisterType<ILogProxyPluginConfiguration, LogProxyPluginConfiguration>();
+            container.RegisterType<IMapsConfiguration, MapsConfiguration>();
+
+            // Other
+            container.RegisterType<IContextProvider, ContextProvider>();
+            container.RegisterType<IDatabaseLogger, DatabaseLogger>();
+            container.RegisterType<IRconClientFactory, RconClientFactory>();
+            container.RegisterType<IRconMonitorFactory, RconMonitorFactory>();
+            container.RegisterType<IPlayerCaching, PlayerCaching>();
+
+            // FM.GeoLocation
+            container.RegisterType<IGeoLocationClientConfiguration, GeoLocationClientConfig>();
+            container.RegisterType<IGeoLocationClient, GeoLocationClient>();
+        }
+    }
+}
