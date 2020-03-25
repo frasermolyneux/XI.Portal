24f13aa6bafafc161a9903b24182ea5796ff37d9
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-25 14:11:57 +0000

    Setting players default to order by last seen desc.

--- src/XI.Portal.Data.Contracts/FilterModels/PlayersFilterModel.cs
+++ src/XI.Portal.Data.Contracts/FilterModels/PlayersFilterModel.cs
@@ -6,7 +6,7 @@ namespace XI.Portal.Data.Contracts.FilterModels
     {
         public GameType GameType { get; set; }
         public FilterType Filter { get; set; } = FilterType.None;
-        public OrderBy Order { get; set; }
+        public OrderBy Order { get; set; } = OrderBy.LastSeenDesc;
         public string FilterString { get; set; }
         public int SkipPlayers { get; set; } = 0;
         public int TakePlayers { get; set; } = 0;
