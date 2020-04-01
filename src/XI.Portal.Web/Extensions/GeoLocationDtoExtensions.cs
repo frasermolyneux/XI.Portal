353ffccbd9f2ccf30c67db926fd081bcc23562d9
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-01 14:39:33 +0100

    Better handling when no country flag and missing data

---
+++ src/XI.Portal.Web/Extensions/GeoLocationDtoExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using FM.GeoLocation.Contract.Models;
+
+namespace XI.Portal.Web.Extensions
+{
+    public static class GeoLocationDtoExtensions
+    {
+        public static HtmlString FlagImage(this GeoLocationDto geoLocationDto)
+        {
+            return !string.IsNullOrWhiteSpace(geoLocationDto.CountryCode)
+                ? new HtmlString($"<img src=\"/content/flags/{geoLocationDto.CountryCode.ToLower()}.png\" />")
+                : new HtmlString("<img src=\"/content/flags/unknown.png\" />");
+        }
+
+        public static HtmlString LocationSummary(this GeoLocationDto geoLocationDto)
+        {
+            if (!string.IsNullOrWhiteSpace(geoLocationDto.CityName) &&
+                !string.IsNullOrWhiteSpace(geoLocationDto.CountryName))
+                return new HtmlString($"{geoLocationDto.CityName}, {geoLocationDto.CountryName}");
+
+            if (!string.IsNullOrWhiteSpace(geoLocationDto.CountryCode))
+                return new HtmlString($"{geoLocationDto.CountryCode}");
+
+            if (!string.IsNullOrWhiteSpace(geoLocationDto.RegisteredCountry))
+                return new HtmlString($"{geoLocationDto.RegisteredCountry}");
+
+            return new HtmlString("Unknown");
+        }
+    }
+}
